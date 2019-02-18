575817c9d730952a0132cc6238eafa770ce5bec1
David Norminton <davidnorminton@gmail.com>
2019-02-18 17:53:32 +0000

    Added netlify.toml file to handle pages when directly accessed

--- nuxt.config.js
+++ nuxt.config.js
@@ -15,7 +15,7 @@ module.exports = {
       { hid: 'description', name: 'description', content: pkg.description }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'icon', type: 'image/x-icon', href: 'https://static.cyberbitnews.com/favicon.ico' },
       { rel: 'stylesheet', href: 'https://static.cyberbitnews.com/css/fontello.css'},
       { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Bitter|Roboto'}  
     ]

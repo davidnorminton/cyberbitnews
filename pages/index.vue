64d0dc8cc880ea40398fdd8ecf171a0be8306f68
David Norminton <davidnorminton@gmail.com>
2019-03-16 16:58:47 +0000

    latest articles showing on index

--- pages/index.vue
+++ pages/index.vue
@@ -1,29 +1,28 @@
 <template lang="pug">
-  .main-container
-    .top-banner
-      .container.padding-top-140
-        .top-banner-text 
-          | Stay upto date with the latest news and stories from
-          | some of the most popular technology, infosec, developer &
-          | linux blogs
-    .goto
-      .container
-        h2 View the latest articles
-        nuxt-link.button.latest-link(to="/latest") all
-        nuxt-link.button.category-link(to="/category/developer") developer
-        nuxt-link.button.category-link(to="/category/infosec") infosec   
-        nuxt-link.button.category-link(to="/category/linux") linux   
-        nuxt-link.button.category-link(to="/category/tech") tech   
-   
+  div       
+    spinner
+    div(v-cloak)  
+    h3.heading.main-heading Latest articles
+    transition(name='outIn')
+      cardView(:news="news", v-if="$store.state.view.mode === 'card'")
+      listView(:news="news", v-else)
 </template>
 
 <script>
 import listView from '@/components/listView'
 import cardView from '@/components/cardView'
+import {article} from '@/middleware/article'
+import spinner from '@/components/spinner'
 
 export default{
-  name:'index',
+  name:'home',
   props: ['id'],
+  mixins: [article],
+  components: {
+    spinner,
+    listView,
+    cardView
+  },
   head () {
     return {
       title: 'cyberbitnews - latest tech, developer, infosec, linux news aggregator',
@@ -39,90 +38,27 @@ export default{
   },
 	data() {
     return {    
+      news: [],
+      page: 1,
+      previous: null,
+      params: null,
+      apiRoute: '/latest.php?page=',
     }
   },
-  watch: { 
+  watch: {
+    '$route'() { 
+      this.params = this.$route.params.id;
+      this.getPage();
+    }  
   },
   mounted() {
+    this.getPage();
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+    this.scroll();
   }
 }
 </script>
 
-<style lang="scss" scoped>
-.main-container {
-  width: 100%;
-  left: 0;
-}
-.main-header {
-  border: 0;
-}
-.content {
-  width: 100%;
-  max-width: 1160px;
-}
-.top-banner {
-  width: 100%;
-  height: 300px;
-}
-header {
-  box-shadow: none;
-  left: -225px;
-}
-.top-banner-text {
-    color: #334d5d;
-    font-size: 3rem;
-    width: 80%;
-    margin: 0 auto;
-    line-height: 3.6rem;
-    font-family: serif;
-    font-style: italic;
-}
-.padding-top-140 {
-  padding-top: 140px;
-}
-.goto {
-  text-align: center;
-  position: relative;
-  margin-top: 60px;
-  padding: 60px 0;
-
-  h2 {
-    font-weight: normal;
-    font-size: 2.6rem;
-    color: #2f3a40;
-    background: white;
-    z-index: 1;
-    display: block;
-    width: 38%;
-    margin: 0 auto;
-    margin-bottom: 80px;
-  }
-  h2::before {
-    content: '';
-    position: absolute;
-    background: #186491;
-    width: 100%;
-    height: 2px;
-    left: 0px;
-    top: 1.5rem;
-    z-index: -1;
-  }
-}
-.button {
-  border-radius: 25rem;
-  padding: 10px 20px;
-  margin: 20px;
-  display: inline-block;
-  text-decoration: none;
-  text-transform: uppercase;
-}
-.latest-link {
-    background: #2f3a40;
-    color: white;
-}
-.category-link {
-    background: #ffa500;
-    color: #2f3a40;
-    font-weight: bold;
-}
+<style lang="scss"> 
+v-clock { display: none}
 </style>
